8d9a5f4f4716060da88957b0edafd5d83ce04127
Sachinmatlani12 <140710422+Sachinmatlani12@users.noreply.github.com>
2023-07-27 16:16:14 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+a=10
+b=20
+c=a=b
+print("add=",c,end=' ')
+print("welcome to TGC")
+
+#Addition of two digits
+a=10
+b=20.5
+c=a+b
+print("add=",c)
+#Multiplication of two digits
+a=1
+b=3
+c=a*b
+print("mul=",c)
+#creating swapping program without using 3rd Variable
+a =10
+b =20
+a,b=b,a
+print("a=",a)
+print("b=",b)
+a,b,c=10,20,30
